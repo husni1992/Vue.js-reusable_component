860af3ece5849a241860148bb686d24fc6c99c37
husny_ahamed <husny.ahamed@workflowsapp.com>
2017-05-21 18:45:54 +0530

    Added: combination of two components into one. And choosing which to display dynamically.

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+var carCounter = {
+    template: '#car-counter',
+    props: ['name', 'initialQuantity'],
+    data: function () {
+        this.$emit('increment', this.initialQuantity);
+        return {
+            quantity: this.initialQuantity
+        }
+    },
+    methods: {
+        increment: function () {
+            this.quantity += 1;
+            this.$emit('increment', 1)
+        }
+    }
+};
+
+var carShow = {
+    template: '#car-show',
+    props: ['name', 'engine']
+};
+
 var card = new Vue({
     el: '#card',
     data: {
         title: 'Cars available in stock',
+        currentView: 'car-show',
         cars: [
             { name: 'Bugati', speed: 387, quantity: 4 },
             { name: 'Lambogini', speed: 290, quantity: 7 },
@@ -9,28 +32,14 @@ var card = new Vue({
             { name: 'Volks Wagon', speed: 285, quantity: 2 }
         ],
         total: null
-    },
+    },    
     methods: {
         incrementTotal: function (amount) {
             this.total += amount;
         }
     },
     components: {
-        'car-counter': {
-            template: '#car-counter',
-            props: ['name', 'initialQuantity'],
-            data: function () {
-                this.$emit('increment', this.initialQuantity);
-                return {
-                    quantity: this.initialQuantity
-                }
-            },
-            methods: {
-                increment: function () {
-                    this.quantity += 1;
-                    this.$emit('increment', 1)
-                }
-            }
-        }
+        'car-counter': carCounter,
+        'car-show': carShow
     }
-})
+});
